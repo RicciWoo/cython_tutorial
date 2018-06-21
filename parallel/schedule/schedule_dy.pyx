6b4d14f638bf077fedaf16e36aa6e15e48943c5d
RicciWoo <ricciwoo@gmail.com>
2018-06-21 10:55:56 -0400

    use 24 threads

--- parallel/schedule/schedule_dy.pyx
+++ parallel/schedule/schedule_dy.pyx
@@ -2,7 +2,7 @@ import numpy as np
 import cython
 from libc.math cimport sqrt, exp
 from cython.parallel import prange
-# cimport openmp
+cimport openmp
 import timeit
 
 @cython.boundscheck(False)
@@ -11,7 +11,7 @@ def array_double(long N, long M):
 	# cdef int num_threads = openmp.omp_get_num_procs()
 	# num_threads = max(2, num_threads//2)
 	# openmp.omp_set_dynamic(0)
-	# openmp.omp_set_num_threads(num_threads)
+	openmp.omp_set_num_threads(24)
 	cdef long i, j
 	cdef double [:, :] inp
 	cdef double [:, :] out
