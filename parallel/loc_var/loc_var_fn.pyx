f566a93c8b437c3c949d0963b9a07ed82be9abf1
RicciWoo <ricciwoo@gmail.com>
2018-07-04 17:21:40 +0800

    update

--- parallel/loc_var/loc_var_fn.pyx
+++ parallel/loc_var/loc_var_fn.pyx
@@ -9,15 +9,17 @@ import timeit
 @cython.wraparound(False)
 
 cdef void loc_fun(double[:, :] inp, double[:, :] out, long i,
-				long M, double *cnt_ptr, double *res_ptr) nogil:
+				long M, double[:] cnt_ptr, double[:] res_ptr) nogil:
 	cdef:
 		long j, k
 		double summ
 		double var[3]
+	cnt_ptr[i] = 0
+	res_ptr[i] = 0
 	for j in range(M):
 		out[i, j] = sqrt(exp(-sqrt(inp[i, j]*(i+j)))) * sqrt(exp(-sqrt(inp[i, j]*(i+j))))
 		# cnt += 1
-		cnt_ptr[0] = cnt_ptr[0] + 1
+		cnt_ptr[i] = cnt_ptr[i] + 1
 		for k in range(3):
 			var[k] = M
 		summ = 0
@@ -25,30 +27,35 @@ cdef void loc_fun(double[:, :] inp, double[:, :] out, long i,
 			# summ += var[k]
 			summ = summ + var[k]
 		# res += summ
-		res_ptr[0] = res_ptr[0] + summ
+		res_ptr[i] = res_ptr[i] + summ
 
 # def array_double(long N, long M):
 def array_double(long N, long M, int num_threads=0):
 	cdef:
 		long i
-		double cnt = 0.0, *cnt_ptr = &cnt
-		double res = 0.0, *res_ptr = &res
+		double cnt = 0.0
+		double[:] cnt_ptr = np.empty((N))
+		double res = 0.0
+		double[:] res_ptr = np.empty((N))
 		double[:, :] inp = np.ones((N, M))
 		double[:, :] out = np.empty((N, M))
-		openmp.omp_lock_t lock
+		# openmp.omp_lock_t lock
 	
 	if num_threads != 0:
 		openmp.omp_set_dynamic(0)
 		openmp.omp_set_num_threads(num_threads)
 	start = timeit.default_timer()
 	with nogil:
-		openmp.omp_init_lock(&lock)
+		# openmp.omp_init_lock(&lock)
 		# for i in range(N):
 		for i in prange(N, schedule='dynamic'):
-			openmp.omp_set_lock(&lock)
+			# openmp.omp_set_lock(&lock)
 			loc_fun(inp, out, i, M, cnt_ptr, res_ptr)
-			openmp.omp_unset_lock(&lock)
-		openmp.omp_destroy_lock(&lock)
+			# openmp.omp_unset_lock(&lock)
+		# openmp.omp_destroy_lock(&lock)
+		for i in range(N):
+			cnt += cnt_ptr[i]
+			res += res_ptr[i]
 	stop = timeit.default_timer()
 	print(stop - start)
 	print(cnt)
